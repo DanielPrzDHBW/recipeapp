9a4cd59ffc0b3a125cd67044668dcf0e4f9ed2b0
Chasedane <chasedane03@gmail.com>
2025-10-02 21:33:09 +0200

    Added basic gin framework
    added a basic gin framework to recive api requests.

--- app/main.go
+++ app/main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/DanielPrzDHBW/recipeapp/api"
+
+	"github.com/gin-gonic/gin"
 )
 
+const port = ":8080"
+
 func main() {
+	router := gin.Default()
+	router.GET("/", api.LandingPage)
 	fmt.Printf("Hello, %s!\n", os.Getenv("USER"))
+
+	router.Run(port) // listen and serve on
 }
