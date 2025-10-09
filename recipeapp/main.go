b21461ebf6d253e6f8b18a762afd7332e053c0f8
DanielPrzDHBW <chasedane03@gmail.com>
2025-10-09 11:16:54 +0200

    Feature/84 requests (#7)
    * [#86] Create endpoints
    
    https://dev.chasedane.com/work_packages/86
    
    * added endpoint for getting recipes as well as creating new a new recipe list
    
    * [#84] Requests
    
    https://dev.chasedane.com/work_packages/84
    
    * Repeat API call
    
    Implemented a loop to call the API 7 times as well as getting rid of any unwanted categories
    
    * Added serverError and models to dockerfile
    
    * added header rules

--- recipeapp/main.go
+++ recipeapp/main.go
@@ -12,7 +12,11 @@ func main() {
 	router := gin.Default()
 	router.GET("/", api.LandingPage) //Provides the frontend of the
 
-	apiGroup := router.Group("/api")            // API group for all API routes
+	apiGroup := router.Group("/api") // API group for all API routes
+	apiGroup.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	})
+
 	apiGroup.GET("/recipes", api.GetRecipes)    // Get a list of saved Recipes from the database by the users cookies
 	apiGroup.GET("/newrecipes", api.NewRecipes) // Get a list of new Recipes from the database by the users cookies
 
