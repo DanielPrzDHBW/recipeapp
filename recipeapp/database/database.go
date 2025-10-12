7a437cfcb3d7e8d67f0bbeb9ac0e0feff906182b
Lukas Herrmann <lukas.herrmann@mail.schwarz>
2025-10-12 14:27:01 +0200

    Add function to write Results to DB

--- recipeapp/database/database.go
+++ recipeapp/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -12,4 +13,26 @@ func ConnectToSQLite() (*gorm.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+type RecipesEntry struct {
+	UUID     string
+	DataJSON string
+}
+
+// Writes a new Result to DB under UUID and returns the uuid, so that it can be stored for access to the data
+func CreateResult(db *gorm.DB, dataJSON string) (string, error) {
+	uuid := generateUUID()
+	entry := RecipesEntry{
+		uuid,
+		dataJSON,
+	}
+	db.Create(&entry)
+	return uuid, nil
+}
+
+// Generates new UUID for new DB entry
+func generateUUID() string {
+	uuid := uuid.New()
+	return uuid.String()
 }
